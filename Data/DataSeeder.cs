cee81bb98468c734faddb1beb2ff6a1a908ef298
Naz <171716584+nazbur05@users.noreply.github.com>
2025-12-14 17:22:44 +0100

    Refactor DataSeeder and update database connection settings

--- Data/DataSeeder.cs
+++ Data/DataSeeder.cs
@@ -1,213 +1,218 @@
-// using ClassroomManagement.Models;
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.EntityFrameworkCore;
+using ClassroomManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-// namespace ClassroomManagement.Data
-// {
-//     public static class DataSeeder
-//     {
-//         public static async Task SeedStudents(UserManager<ApplicationUser> userManager)
-//         {
-//             var students = new[]
-//             {
-//                 new { User = new ApplicationUser
-//                         {
-//                             StudId = "w69645",
-//                             UserName = "gulzatyessen@gmail.com",
-//                             Email = "gulzatyessen@gmail.com",
-//                             FirstName = "Gulzat",
-//                             LastName = "Yessen",
-//                             EmailConfirmed = true
-//                         },
-//                     Password = "Gulzatpswd1"
-//                 },
-//                 new { User = new ApplicationUser
-//                         {
-//                             StudId = "w70963",
-//                             UserName = "nazburgenbay01@gmail.com",
-//                             Email = "nazburgenbay01@gmail.com",
-//                             FirstName = "Naz",
-//                             LastName = "Burgenbay",
-//                             EmailConfirmed = true
-//                         },
-//                     Password = "Nazpswd1"
-//                 },
-//                 new { User = new ApplicationUser
-//                         {
-//                             StudId = "w69646",
-//                             UserName = "dilnazdosumbek7@gmail.com",
-//                             Email = "dilnazdosumbek7@gmail.com",
-//                             FirstName = "Dilnaz",
-//                             LastName = "Dosumbek",
-//                             EmailConfirmed = true
-//                         },
-//                     Password = "Dilnazpswd1"
-//                 }
-//             };
+namespace ClassroomManagement.Data
+{
+    public static class DataSeeder
+    {
+        public static async Task SeedStudents(UserManager<ApplicationUser> userManager)
+        {
+            var students = new[]
+            {
+                new { User = new ApplicationUser
+                        {
+                            StudId = "w69645",
+                            UserName = "gulzatyessen@gmail.com",
+                            Email = "gulzatyessen@gmail.com",
+                            FirstName = "Gulzat",
+                            LastName = "Yessen",
+                            EmailConfirmed = true
+                        },
+                    Password = "Gulzatpswd1"
+                },
+                new { User = new ApplicationUser
+                        {
+                            StudId = "w70963",
+                            UserName = "nazburgenbay01@gmail.com",
+                            Email = "nazburgenbay01@gmail.com",
+                            FirstName = "Naz",
+                            LastName = "Burgenbay",
+                            EmailConfirmed = true
+                        },
+                    Password = "Nazpswd1"
+                },
+                new { User = new ApplicationUser
+                        {
+                            StudId = "w69646",
+                            UserName = "dilnazdosumbek7@gmail.com",
+                            Email = "dilnazdosumbek7@gmail.com",
+                            FirstName = "Dilnaz",
+                            LastName = "Dosumbek",
+                            EmailConfirmed = true
+                        },
+                    Password = "Dilnazpswd1"
+                }
+            };
 
-//             foreach (var studentData in students)
-//             {
-//                 var user = await userManager.FindByEmailAsync(studentData.User.Email);
-//                 if (user == null)
-//                 {
-//                     var result = await userManager.CreateAsync(studentData.User, studentData.Password);
-//                     if (result.Succeeded)
-//                     {
-//                         await userManager.AddToRoleAsync(studentData.User, "Student");
-//                     }
-//                     else
-//                     {
-//                         Console.WriteLine($"Failed to create student {studentData.User.Email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
-//                     }
-//                 }
-//             }
-//         }
+            foreach (var studentData in students)
+            {
+                var user = await userManager.FindByEmailAsync(studentData.User.Email);
+                if (user == null)
+                {
+                    var result = await userManager.CreateAsync(studentData.User, studentData.Password);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(studentData.User, "Student");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to create student {studentData.User.Email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                    }
+                }
+            }
+        }
 
-//         public static async Task SeedInstructors(UserManager<ApplicationUser> userManager)
-//         {
-//             var instructors = new[]
-//             {
-//                 new { User = new ApplicationUser
-//                         {
-//                             UserName = "mjagiela@classroom.com",
-//                             Email = "mjagiela@classroom.com",
-//                             FirstName = "Marcin",
-//                             LastName = "Jagiela",
-//                             EmailConfirmed = true
-//                         },
-//                       Password = "Mjagielapswd1"
-//                 },
-//                 new { User = new ApplicationUser
-//                         {
-//                             UserName = "mknap@classroom.com",
-//                             Email = "mknap@classroom.com",
-//                             FirstName = "Maksymilian",
-//                             LastName = "Knap",
-//                             EmailConfirmed = true
-//                         },
-//                       Password = "Mknappswd1"
-//                 }
-//             };
+        public static async Task SeedInstructors(UserManager<ApplicationUser> userManager)
+        {
+            var instructors = new[]
+            {
+                new { User = new ApplicationUser
+                        {
+                            UserName = "mjagiela@classroom.com",
+                            Email = "mjagiela@classroom.com",
+                            FirstName = "Marcin",
+                            LastName = "Jagiela",
+                            EmailConfirmed = true
+                        },
+                      Password = "Mjagielapswd1"
+                },
+                new { User = new ApplicationUser
+                        {
+                            UserName = "mknap@classroom.com",
+                            Email = "mknap@classroom.com",
+                            FirstName = "Maksymilian",
+                            LastName = "Knap",
+                            EmailConfirmed = true
+                        },
+                      Password = "Mknappswd1"
+                }
+            };
 
-//             foreach (var instructorData in instructors)
-//             {
-//                 var user = await userManager.FindByEmailAsync(instructorData.User.Email);
-//                 if (user == null)
-//                 {
-//                     var result = await userManager.CreateAsync(instructorData.User, instructorData.Password);
-//                     if (result.Succeeded)
-//                     {
-//                         await userManager.AddToRoleAsync(instructorData.User, "Instructor");
-//                     }
-//                     else
-//                     {
-//                         Console.WriteLine($"Failed to create user {instructorData.User.Email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
-//                     }
-//                 }
-//             }
-//         }
+            foreach (var instructorData in instructors)
+            {
+                var user = await userManager.FindByEmailAsync(instructorData.User.Email);
+                if (user == null)
+                {
+                    var result = await userManager.CreateAsync(instructorData.User, instructorData.Password);
+                    if (result.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(instructorData.User, "Instructor");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Failed to create user {instructorData.User.Email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+                    }
+                }
+            }
+        }
 
-//         public static async Task SeedCourses(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
-//         {
-//             // await context.Database.EnsureCreatedAsync();
+        public static async Task SeedCourses(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            // await context.Database.EnsureCreatedAsync();
 
-//             // Get instructors seeded earlier
-//             var instructor1 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "mjagiela@classroom.com");
-//             var instructor2 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "mknap@classroom.com");
+            // Get instructors seeded earlier
+            var instructor1 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "mjagiela@classroom.com");
+            var instructor2 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "mknap@classroom.com");
 
-//             if (instructor1 == null || instructor2 == null)
-//             {
-//                 throw new Exception("Instructors must be seeded before courses.");
-//             }
+            if (instructor1 == null || instructor2 == null)
+            {
+                throw new Exception("Instructors must be seeded before courses.");
+            }
 
-//             if (!context.Courses.Any())
-//             {
-//                 var courses = new[]
-//                 {
-//                     new Course
-//                     {
-//                         Name = "Programming Languages",
-//                         InstructorId = instructor1.Id,
-//                         Description = "Basics of .NET"
-//                     },
-//                     new Course
-//                     {
-//                         Name = "Software Engineering",
-//                         InstructorId = instructor2.Id,
-//                         Description = "All about the Software Development cycle"
-//                     }
-//                 };
+            if (!context.Courses.Any())
+            {
+                var courses = new[]
+                {
+                    new Course
+                    {
+                        Name = "Programming Languages",
+                        InstructorId = instructor1.Id,
+                        Description = "Basics of .NET"
+                    },
+                    new Course
+                    {
+                        Name = "Software Engineering",
+                        InstructorId = instructor2.Id,
+                        Description = "All about the Software Development cycle"
+                    }
+                };
 
-//                 context.Courses.AddRange(courses);
-//                 await context.SaveChangesAsync();
-//             }
+                context.Courses.AddRange(courses);
+                await context.SaveChangesAsync();
+            }
 
-//             if (!context.CourseMaterials.Any())
-//             {
-//                 var introCourse = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Programming Languages");
-//                 var algoCourse = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Software Engineering");
+            if (!context.CourseMaterials.Any())
+            {
+                var introCourse = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Programming Languages");
+                var algoCourse = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Software Engineering");
 
-//                 var materials = new[]
-//                 {
-//                     new CourseMaterial
-//                     {
-//                         CourseId = introCourse.Id,
-//                         InstructorId = instructor1.Id,
-//                         Title = "Lecture 1: Getting Started",
-//                         Content = "Welcome to programming!",
-//                         FilePath = null
-//                     },
-//                     new CourseMaterial
-//                     {
-//                         CourseId = algoCourse.Id,
-//                         InstructorId = instructor2.Id,
-//                         Title = "Lecture 1: Types of requirements",
-//                         Content = "Will add .pdf file soon",
-//                         FilePath = null
-//                     }
-//                 };
+                if (introCourse == null || algoCourse == null)
+                {
+                    throw new Exception("Courses must be seeded before course materials.");
+                }
 
-//                 context.CourseMaterials.AddRange(materials);
-//                 await context.SaveChangesAsync();
-//             }
-//         }
+                var materials = new[]
+                {
+                    new CourseMaterial
+                    {
+                        CourseId = introCourse.Id,
+                        InstructorId = instructor1.Id,
+                        Title = "Lecture 1: Getting Started",
+                        Content = "Welcome to programming!",
+                        FilePath = null
+                    },
+                    new CourseMaterial
+                    {
+                        CourseId = algoCourse.Id,
+                        InstructorId = instructor2.Id,
+                        Title = "Lecture 1: Types of requirements",
+                        Content = "Will add .pdf file soon",
+                        FilePath = null
+                    }
+                };
 
-//         public static async Task SeedStudentCourses(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
-//         {
-//             // await context.Database.EnsureCreatedAsync();
+                context.CourseMaterials.AddRange(materials);
+                await context.SaveChangesAsync();
+            }
+        }
 
-//             var student1 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "gulzatyessen@gmail.com");
-//             var student2 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "nazburgenbay01@gmail.com");
-//             var student3 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "dilnazdosumbek7@gmail.com");
+        public static async Task SeedStudentCourses(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            // await context.Database.EnsureCreatedAsync();
 
-//             if (student1 == null || student2 == null || student3 == null)
-//             {
-//                 throw new Exception("Students must be seeded before assigning courses.");
-//             }
+            var student1 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "gulzatyessen@gmail.com");
+            var student2 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "nazburgenbay01@gmail.com");
+            var student3 = await userManager.Users.FirstOrDefaultAsync(u => u.Email == "dilnazdosumbek7@gmail.com");
 
-//             var course1 = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Programming Languages");
-//             var course2 = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Software Engineering");
+            if (student1 == null || student2 == null || student3 == null)
+            {
+                throw new Exception("Students must be seeded before assigning courses.");
+            }
 
-//             if (course1 == null || course2 == null)
-//             {
-//                 throw new Exception("Courses must be seeded before assigning students.");
-//             }
+            var course1 = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Programming Languages");
+            var course2 = await context.Courses.FirstOrDefaultAsync(c => c.Name == "Software Engineering");
 
-//             if (!context.StudentCourses.Any())
-//             {
-//                 var studentCourses = new[]
-//                 {
-//                     new StudentCourse { StudentId = student1.Id, CourseId = course1.Id },
-//                     new StudentCourse { StudentId = student1.Id, CourseId = course2.Id },
-//                     new StudentCourse { StudentId = student2.Id, CourseId = course1.Id },
-//                     new StudentCourse { StudentId = student2.Id, CourseId = course2.Id },
-//                     new StudentCourse { StudentId = student3.Id, CourseId = course1.Id },
-//                     new StudentCourse { StudentId = student3.Id, CourseId = course2.Id },
-//                 };
+            if (course1 == null || course2 == null)
+            {
+                throw new Exception("Courses must be seeded before assigning students.");
+            }
 
-//                 context.StudentCourses.AddRange(studentCourses);
-//                 await context.SaveChangesAsync();
-//             }
-//         }
-//     }
-// }
+            if (!context.StudentCourses.Any())
+            {
+                var studentCourses = new[]
+                {
+                    new StudentCourse { StudentId = student1.Id, CourseId = course1.Id },
+                    new StudentCourse { StudentId = student1.Id, CourseId = course2.Id },
+                    new StudentCourse { StudentId = student2.Id, CourseId = course1.Id },
+                    new StudentCourse { StudentId = student2.Id, CourseId = course2.Id },
+                    new StudentCourse { StudentId = student3.Id, CourseId = course1.Id },
+                    new StudentCourse { StudentId = student3.Id, CourseId = course2.Id },
+                };
+
+                context.StudentCourses.AddRange(studentCourses);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
